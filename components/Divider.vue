0679f042149e0a52be80ac22f7f8972780b156bb
Alexis Cousins <alexis.simmons1997@gmail.com>
2021-10-30 18:22:49 +1300

    Added divider component

---
+++ components/Divider.vue
@@ -0,0 +1,36 @@
+<template>
+  <svg
+    width="124"
+    height="22"
+    viewBox="0 0 124 22"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    class="mx-auto mt-2"
+  >
+    <path
+      d="M15 11L107 11M10.5 4C14.6421 4 18 7.35786 18 11.5C18 15.6421 14.6421 19 10.5 19C6.35786 19 3 15.6421 3 11.5C3 7.35786 6.35786 4 10.5 4ZM113.5 3C117.642 2.99999 121 6.35786 121 10.5C121 14.6421 117.642 18 113.5 18C109.358 18 106 14.6421 106 10.5C106 6.35786 109.358 3 113.5 3Z"
+      stroke="url(#paint0_linear_6:22)"
+      stroke-width="6"
+    />
+    <defs>
+      <linearGradient
+        id="paint0_linear_6:22"
+        x1="3"
+        y1="11"
+        x2="121"
+        y2="11"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#ED0E4A" />
+        <stop offset="1" stop-color="#7263BF" />
+      </linearGradient>
+    </defs>
+  </svg>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
