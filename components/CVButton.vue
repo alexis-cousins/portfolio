2ccda909811a6a1d7dab2a4b0cd2eed7ee4f278e
Alexis Cousins <alexis.simmons1997@gmail.com>
2021-11-09 17:18:43 +1300

    Skills Page base complete

---
+++ components/CVButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button
+    @click="$emit('click')"
+    class="
+      font-bold
+      w-40
+      text-portfolio-dark-grey
+      uppercase
+      bg-gradient-to-r
+      from-red-500
+      via-pink-500
+      to-indigo-500
+      pt-1
+      pb-0.5
+      rounded-lg
+    "
+  >
+    View CV
+  </button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ components/Skillbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div :class="`${skill.width} my-6`">
+    <div class="flex justify-between my-2 font-semibold">
+      <h1>{{ skill.name }}</h1>
+      <h1>{{ skill.confidence }}</h1>
+    </div>
+    <div
+      class="
+        rounded-full
+        h-3
+        bg-gradient-to-r
+        from-red-500
+        via-pink-500
+        to-indigo-500
+      "
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    skill: {
+      type: Object,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
