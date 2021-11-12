8de1cb03bbee3d03976f23bc09081da9b7620712
Alexis Cousins <alexis.simmons1997@gmail.com>
2021-11-12 20:16:15 +1300

    Hubspot form added

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,41 +1,86 @@
 <template>
   <div>
-    <form @submit.prevent="() => console.log(e)" class="flex flex-col">
+    <form @submit.prevent="submit" class="flex flex-col">
       <div class="flex flex-col space-y-1">
         <label for="name">Name:</label>
         <input
+          v-model="name"
           type="text"
-          id="first-name"
+          id="name"
           name="name"
-          class="bg-portfolio-light-grey rounded-md h-8"
+          class="bg-portfolio-light-grey rounded-md h-8 py-0.5 px-1"
           required
         />
       </div>
       <div class="flex flex-col space-y-1 mt-2">
-        <label for="contact-number">Contact Number:</label>
+        <label for="email">Email:</label>
         <input
-          type="number"
-          id="contact-number"
-          name="contact-number"
-          class="bg-portfolio-light-grey rounded-md h-8"
+          v-model="email"
+          type="email"
+          id="email"
+          name="email"
+          class="bg-portfolio-light-grey rounded-md h-8 py-0.5 px-1"
           required
         />
       </div>
       <div class="flex flex-col space-y-1 mt-2">
         <label for="enquiry">Enquiry:</label>
         <textarea
+          v-model="enquiry"
           name="enquiry"
           id="enquiry-field"
           cols="30"
           rows="6"
-          class="bg-portfolio-light-grey rounded-md"
+          class="bg-portfolio-light-grey rounded-md py-0.5 px-1"
         ></textarea>
       </div>
-      <ContactButton class="mt-4 mx-auto" />
+      <ContactButton class="mt-10 mx-auto" />
     </form>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      name: "",
+      email: "",
+      enquiry: "",
+    };
+  },
+  methods: {
+    async submit() {
+      try {
+        let response = await this.$axios.post(
+          "https://api.hsforms.com/submissions/v3/integration/submit/21004862/e50be665-5d1a-4e27-ac3a-bec2157832b8",
+          {
+            fields: {
+              name: this.name,
+              email: this.email,
+              enquiry: this.enquiry,
+            },
+            fields: [
+              {
+                name: "email",
+                value: this.email,
+              },
+              {
+                name: "firstname",
+                value: this.name,
+              },
+              {
+                name: "message",
+                value: this.enquiry,
+              },
+            ],
+          }
+        );
+
+        alert("Success!");
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+};
 </script>
