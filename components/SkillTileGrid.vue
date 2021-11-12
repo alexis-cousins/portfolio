c5bc4e8e8a638511305a55d106ef9ecfaf060520
Alexis Cousins <alexis.simmons1997@gmail.com>
2021-11-12 20:18:21 +1300

    Skill tile grid component created

---
+++ components/SkillTileGrid.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="md:w-4/5 lg:w-3/5 mx-auto grid grid-cols-2 md:grid-cols-3 gap-6">
+    <SkillTile v-for="(skill, index) in skills" :key="index" :skill="skill" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    skills: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+<style>
+</style>
