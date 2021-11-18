5ac0917e535cd00b247edf8a8766a515dff6b58d
Alexis Cousins <alexis.simmons1997@gmail.com>
2021-11-18 16:12:27 +1300

    Site name changed

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "AC-portfolio",
+    title: "Alexis Cousins: Developer",
     htmlAttrs: {
       lang: "en",
     },
